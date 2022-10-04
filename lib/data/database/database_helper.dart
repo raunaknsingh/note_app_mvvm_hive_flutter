a7c557e83883ba18c2375c94ca22e93a6959c1a4
raunak <raunak.singh@getir.com>
2022-10-04 13:34:59 +0530

    Replaced Future<void> to Future

--- lib/data/database/database_helper.dart
+++ lib/data/database/database_helper.dart
@@ -7,7 +7,7 @@ class DatabaseHelper {
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
-  Future<void> insertNote(NoteEntity note) async {
+  Future insertNote(NoteEntity note) async {
     await Boxes.getNotesBox().put(note.id, note);
   }
 
@@ -15,11 +15,11 @@ class DatabaseHelper {
     return Boxes.getNotesBox().values.toList().cast<NoteEntity>();
   }
 
-  Future<void> updateNote(NoteEntity note) async {
+  Future updateNote(NoteEntity note) async {
     Boxes.getNotesBox().put(note.id, note);
   }
 
-  Future<void> deleteNote(int id) async {
+  Future deleteNote(int id) async {
     Boxes.getNotesBox().delete(id);
   }
 }
